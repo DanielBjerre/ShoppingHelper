597a59674e08932a8f09e08197528ff792b2dbd7
Daniel Bjerre Madsen <danielbmadsen93@gmail.com>
2024-01-05 23:55:40 +0100

    rename project

---
+++ ScrapeUrlsConsumer/Scraper/IUrlScraperService.cs
@@ -0,0 +1,6 @@
+﻿namespace ScrapeUrlsConsumer.Scraper;
+internal interface IUrlScraperService
+{
+    public string RecipeSource { get; set; }
+    public IEnumerable<Uri> ScrapeUrls { get; set; }
+}
